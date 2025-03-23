9a3e794d2c7982416abccb066326ba172aa9a210
arielthemonke <arielsaks1@gmail.com>
2025-03-23 17:14:54 +0200

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using BepInEx;
+using UnityEngine;
+
+namespace GTPlugin
+{
+    // This is the main plugin class. It is the entry point of the plugin, put your main code here
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.ProjectName, PluginInfo.Version)]
+    public class Plugin
+    {
+        bool inAllowedRoom;
+        void Start()
+        {
+            // Just use OnGameInitialized() it is better
+            Utilla.Events.GameInitialized += OnGameInitialized;
+        }
+
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            // This is called when the game is initialized
+            Debug.Log("This example mod has been loaded!");
+        }
+
+        void Update()
+        {
+            // This is called every frame
+            if (NetworkSystem.Instance.GameModeString.Contains("MODDED"))
+            {
+                inAllowedRoom = true;
+            }
+            else
+            {
+                inAllowedRoom = false;
+            }
+
+
+            if (inAllowedRoom == true)
+            {
+                // Activate your mod here
+            }
+            else
+            {
+                // Deactivate your mod here
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GTPlugin
+{
+
+    // This class is used to store information about the plugin. Please fill in the fields below.
+    class PluginInfo
+    {
+        public const string GUID = "authorname.projectname";
+        public const string ProjectName = "GorillaTagModTemplate";
+        public const string Version = "1.0.0";
+        public const string Description = "An example description";
+    }
+}
